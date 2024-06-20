ac8eb0b694e28e19826ada4addc35dd2a2a315a6
juliadelmar <149151461+juliadelmar@users.noreply.github.com>
2024-06-20 09:06:04 +0200

    Update ConsumidorApplicationTests.java

--- consumidor/src/test/java/com/app_usuarios/ConsumidorApplicationTests.java
+++ consumidor/src/test/java/com/app_usuarios/ConsumidorApplicationTests.java
@@ -1,13 +1,15 @@
 package com.app_usuarios;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConsumidorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertTrue(true, "Funciona");
+    }
 }
